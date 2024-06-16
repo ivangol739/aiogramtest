c4f5bce2bd12643740e3b1bc5345ccb12bfe66c1
ivangol739 <ivangol739@gmail.com>
2024-06-16 22:11:08 +0300

    magicfiltres

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import asyncio
 import logging
 
 from dotenv import load_dotenv
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
@@ -21,9 +21,37 @@ async def cmd_start(message: Message):
   #                              caption='Картинка')
   
   
-@dp.message()
+@dp.message(F.text == "привет")
 async def echo(message: Message):
-    await message.answer('Это неизвестная команда.')
+  await message.answer('Привет!!!')
+  await message.answer(f'Ваш ID: {message.from_user.id}')
+
+
+@dp.message(F.photo)
+async def echo(message: Message):
+  photo_id = message.photo[-1].file_id
+  await message.answer_photo(photo=photo_id)
+  
+  
+@dp.message(F.document)
+async def echo(message: Message):
+  await message.answer_document(document=message.document.file_id)
+  
+  
+@dp.message(F.audio)
+async def echo(message: Message):
+  await message.answer_audio(audio=message.audio.file_id)
+  
+  
+@dp.message(F.animation)
+async def echo(message: Message):
+  await message.answer_animation(animation=message.animation.file_id)
+  await message.answer("Вы прислали Гифку")
+  
+
+@dp.message(F.from_user.id == 293882390)
+async def echo(message: Message):
+  await message.answer(f"Написал юзер с ID {message.from_user.id}")
 
 
 async def main():
