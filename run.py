cf402d4724cbae8a1de4c91e1d76e5110b4f5e3d
ivangol739 <ivangol739@gmail.com>
2024-06-24 21:41:09 +0300

    commandobject

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import logging
 
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, F
-from aiogram.filters import CommandStart, Command
+from aiogram.filters import CommandStart, Command, CommandObject
 from aiogram.types import Message
 
 load_dotenv()
@@ -24,6 +24,18 @@ async def cmd_start(message: Message):
 @dp.message(Command('help'))
 async def cmd_help(message: Message):
   await message.answer(f"{message.from_user.first_name}, вам нужна помощь ?")
+  
+  
+@dp.message(Command("get"))
+async def cmd_get(message: Message, command: CommandObject):
+  if not command.args:
+    await message.answer("Аргументы не переданы")
+    return
+  try:
+    value1, value2 = command.args.split(' ', maxsplit=1)
+    await message.answer(f"Вы ввели команду get с аргументом {value1} {value2}")
+  except:
+    await message.answer("Были введены неправильные аргументы")
   
   
 @dp.message(F.text == "привет")
